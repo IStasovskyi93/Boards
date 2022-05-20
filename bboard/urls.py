9bccc669bb51b34e60973fe8a37eda04a627a6c8
Ihor_Stasovskyi <stsovsky400m@gmail.com>
2022-05-20 12:15:23 +0200

    create form and template for add board

--- bboard/urls.py
+++ bboard/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
-    path('<int:rubric_id>/', by_rubric),
+    path('', index, name='home'),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('add_board/', BoardCreateView.as_view(), name='add_board')
 ]
--- bboard/views.py
+++ bboard/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
 from .models import *
+from .forms import *
 
 
 def index(request):
     boards = Board.objects.all()
-    context = {'boards': boards}
+    rubrics = Rubric.objects.all()
+    context = {'boards': boards, 'rubrics': rubrics}
     return render(request, 'bboard/index.html', context)
 
 
@@ -17,3 +22,14 @@ def by_rubric(request, rubric_id):
     current_rubric = Rubric.objects.get(pk=rubric_id)
     context = {'boards': boards, 'rubrics': rubrics, 'current_rubric': current_rubric}
     return render(request, 'bboard/by_rubric.html', context)
+
+
+class BoardCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BoardForm
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubric'] = Rubric.objects.all()
+        return context
